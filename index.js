4ffdc0176fe62ee8a92f778ad848310a35410ea5
wangyan <wyvernnot@gmail.com>
2015-10-27 16:01:23 +0800

    Deal with events

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 /**
  * Created by yan on 15-10-27.
  */
+var EventEmitter = require('events').EventEmitter;
+var http = require('http');
+var request = require('request');
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({
+  name: 'registor'
+});
+
+var eventSource = new EventEmitter();
+
+function errorHandler() {
+  log.error(arguments);
+  eventSource.emit.apply(eventSource, 'error', arguments);
+}
+
+var req = http.request({
+  path: '/events',
+  socketPath: '/var/run/docker.sock'
+}, function (res) {
+  log.info('connected to docker daemon %s', res.req.socketPath);
+  log.info('headers.server %s', res.headers.server);
+
+  res.on('error', errorHandler);
+  res.on('data', function (buf) {
+    var eventObject = JSON.parse(buf.toString());
+    log.info('event', eventObject);
+
+    var url = 'http://unix:/var/run/docker.sock:/containers/' + eventObject.id + '/json';
+    log.info('request container information', {
+      url: url
+    });
+
+    request.get(url, function (error, response, body) {
+      log.info('receive container information', {
+        statusCode: response.statusCode
+      });
+
+      if (response.statusCode == '200') {
+        try {
+          eventObject.payLoad = JSON.parse(body);
+          eventSource.emit('event', eventObject);
+          log.trace('event', eventObject);
+        } catch (error) {
+          eventSource.emit('error', body);
+        }
+      } else {
+        eventSource.emit('error', body);
+      }
+    });
+  });
+});
+
+req.on('error', errorHandler);
+
+req.end();
+
+module.exports = eventSource;
--- server.js
+++ server.js
@@ -4,18 +4,46 @@
 var express = require('express');
 var httpProxy = require('http-proxy');
 var path = require('path');
+var WebSocketServer = require('ws').Server;
+var bunyan = require('bunyan');
 
+var log = bunyan.createLogger({
+  name: 'registor-server'
+});
+var server = require('http').createServer();
+var wss = new WebSocketServer({server: server});
 var proxy = httpProxy.createProxyServer({
   target: {
     socketPath: '/var/run/docker.sock'
   }
 });
-
 var app = express();
+var dockerEvents = require('./');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/docker', function (req, res) {
   proxy.web(req, res);
 });
 
-app.listen(3000);
+dockerEvents.on('event', function (data) {
+  log.info('broadcasting to %d clents', wss.clients.length);
+  wss.clients.forEach(function (client) {
+    client.send(JSON.stringify(data));
+  });
+});
+
+wss.on('connection', function (c) {
+  var remoteAddress = {
+    remotePort: c._socket.remotePort,
+    remoteFamily: c._socket.remoteFamily,
+    remoteAddress: c._socket.remoteAddress
+  }
+  log.info('connection', remoteAddress);
+})
+
+server.on('request', app);
+
+server.listen(3000, function () {
+  log.info('listening on', server.address().port);
+});
